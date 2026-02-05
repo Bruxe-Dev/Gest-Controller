0f6c9e8a830f5057698b1d7b9e05209afd2a8d84
Bruxe-Dev <mnibeza23@gmail.com>
2026-02-05 14:58:31 +0200

    finished creating the main hand tracker module that handles webcam input and hand detection and renamed the file module for easy understanding

--- hand_tracker.py
+++ hand_tracker.py
@@ -93,4 +93,27 @@ class HandTracker:
 
         return distance < threshold
 
+    def count_fingers_up(self,hand_index = 0,)-> int:
+        landmarks = slef.get_all_landmarks(hand_index)
+
+        if not landmarks:
+            return 0
+        
+        fingers_up = 0
+
+        #Checks whether the thumb is in a horizontal position
+
+        if landmarks[4][0] < landmarks[3][0]:
+            fingers_up += 1
+
+        finger_tip = [8, 12, 16 ,20]
+        finger_joints = [6, 10, 14, 16]
+
+        for tip, joint in zip(finger_tips, finger_joints):
+            if landmarks[tip][1] < landmarks[joint][1]:  # Tip is above joint
+                fingers_up += 1
+
+        return fingers_up
     
+    def release(self):
+        self.hands.close()
