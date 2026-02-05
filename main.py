8eb7c5ed7c6c4f78ee4d2354b1d407fa98a7ef5b
Bruxe-Dev <mnibeza23@gmail.com>
2026-02-05 13:28:10 +0200

    finished the main init function to be used to controll the tracking and dewtection methods

--- main.py
+++ main.py
@@ -2,3 +2,22 @@ import cv2
 import mediapipe as mediapipe
 import numpy as np
 from typing import Optional, Tuple ,List
+
+class HandTracker:
+    """This will help track hand landmarks nad provide gesture detection capabilities"""
+    
+    def __init__(self, max_hands=1,min_detection_confidence=0.75, min_tracking_confidence=0.5):
+        self.mp_hands = mp.solutions.hands
+        self.mp_drawing = mp.solutions.drawing_utils
+
+        self.hands =self.mp.hands.Hands(
+            static_image_mode = False,
+            max_num_hands = num_hands,
+            min_detection_confidence = min_detection_confidence,
+            min_tracking_confidence = min_tracking_confidence
+        )
+
+        self.results =None
+        self.frame_shape =None
+    
+    
